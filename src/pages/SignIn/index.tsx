e904bca6da87438e495719244faa69e699b94673
cazuzaneves <cazuzaneves@gmail.com>
2020-06-06 14:06:03 -0300

    input & button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Image } from "react-native";
 import { Container, Title } from "./styles";
 
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
 import logoImg from "../../assets/logo.png";
 
 export default () => {
@@ -9,6 +12,11 @@ export default () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Password" />
+
+      <Button>Entrar</Button>
     </Container>
   );
 };
