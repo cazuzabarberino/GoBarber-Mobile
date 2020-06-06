45de2359baa7d44187752a5d36ad730d618a2789
cazuzaneves <cazuzaneves@gmail.com>
2020-06-06 12:39:28 -0300

    pixel density

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Image } from "react-native";
 import { Container } from "./styles";
 
+import logoImg from '../../assets/logo.png'
+
 export default () => {
-  return <Container/>
+  return (
+    <Container>
+      <Image source={logoImg} />
+    </Container>
+  );
 };
