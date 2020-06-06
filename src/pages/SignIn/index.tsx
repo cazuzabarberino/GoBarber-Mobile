b638c18da37e38b31da51a538f9c9c210efc53eb
cazuzaneves <cazuzaneves@gmail.com>
2020-06-06 12:50:46 -0300

    exporting fonts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Image } from "react-native";
-import { Container } from "./styles";
+import { Container, Title } from "./styles";
 
-import logoImg from '../../assets/logo.png'
+import logoImg from "../../assets/logo.png";
 
 export default () => {
   return (
     <Container>
       <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
     </Container>
   );
 };
